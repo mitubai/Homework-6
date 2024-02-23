538f8a3d2cc69152edd8721ee6a49588742e3f7e
mitubai <akimzhanovmitu2011@gmail.com>
2024-02-23 11:39:31 +0600

    ns

--- class1.py
+++ class1.py
@@ -2,7 +2,7 @@ class Hello:
     def __init__(self, str):
         self.str = str
 class NewClass:
-    word = 'KOtokBAs'  # Class-level attribute to store the word
+    word = ''  # Class-level attribute to store the word
 
     @classmethod
     def set_word(cls, word):
