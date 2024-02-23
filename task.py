ff4a610201577eb39e9723744d953e3c2dcd2e82
mitubai <akimzhanovmitu2011@gmail.com>
2024-02-23 11:35:08 +0600

    BS

--- task.py
+++ task.py
@@ -1,3 +1,10 @@
 from art import tprint
 tprint("PYTHON")
+from art import tprint
+tprint("PYTHON")
+from class1 import NewClass
+NewClass.set_word(word='BrawlStars')
+tprint(NewClass.current_word())
+
+
 
